e5d872a9d4fa2ccb7aec53ebdff644359626348e
jewe1V <zensot1@yandex.ru>
2026-05-19 22:17:15 +0500

    feat: header component

---
+++ app/components/header/header.tsx
@@ -0,0 +1,50 @@
+import "./header.css"
+import {Bell, ChevronDown, Handbag, Search} from "lucide-react";
+
+export const Header = () => {
+    return (
+        <header className="header">
+            <div className="header__top">
+                <a href="#" className="header__logo">
+                    <img src="/images/logo.svg" alt="logo" />
+                    ShopAI
+                </a>
+
+
+
+                <div className="header__search">
+                    <button className="header__catalog-btn">
+                        <ChevronDown size={21}/>
+                        Каталог
+                    </button>
+                    <input type="text" className="header__search-input" placeholder="Найти товар или магазин..." />
+                    <div className="header__search-icon">
+                        <Search size={15} color={"#99A1AF"} />
+                    </div>
+                </div>
+
+                <div className="header__actions">
+                    <button className="header__ai-btn">
+                        ИИ-помощник
+                        <Search size={14} color={"#2563eb"} />
+                    </button>
+                    <button className="header__icon-btn">
+                        <Bell size={24} color={"#4A5565"}/>
+                    </button>
+                    <button className="header__icon-btn">
+                        <Handbag size={24} color={"#4A5565"}/>
+                        <span className="header__badge">3</span>
+                    </button>
+                    <div className="header__avatar">П</div>
+                </div>
+            </div>
+
+            {/* Navigation */}
+            <nav className="nav">
+                <a href="#" className="nav__link nav__link--active">Главная</a>
+                <a href="#" className="nav__link">Мои заказы</a>
+                <a href="#" className="nav__link">Избранное</a>
+            </nav>
+        </header>
+    )
+}
